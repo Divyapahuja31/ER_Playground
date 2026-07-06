c586ddb5824e88327b44b6caadf00f08138a6b51
Kanishk Ranjan <kanishkranjan17@gmail.com>
2026-07-06 15:59:01 +0530

    feat: connects add node to reactflow

--- app/components/sidebarInnerView.tsx
+++ app/components/sidebarInnerView.tsx
@@ -1,9 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNodes,useReactFlow } from '@xyflow/react'
 
 import SideBarNode from "./sidebarNode"
 
 const SideInnerView = ({ name }: { name: string }) => {
-  const [nodes, setNodes] = useState<[]>([])
+  const data = useNodes();
+  const { addNodes } = useReactFlow();
+  const [nodes, setNodes] = useState(data)
+
+  useEffect(()=>{
+    console.log(data)
+  },[])
+
+  const handleOnClick = () => {
+    const id = `${Date.now()}`;
+    const newNode = {
+      id,
+      type: 'default',
+      data: { label: `Node ${id.slice(-4)}` },
+      position: { x:0, y: 0 },
+    };
+    addNodes(newNode);
+  }
 
   return (
     <div className='m-5'>
@@ -13,7 +31,7 @@ const SideInnerView = ({ name }: { name: string }) => {
           type="text"
           placeholder='Search.....'
         />
-        <button>
+        <button onClick={handleOnClick}>
           Add {name}
         </button>
       </div>
