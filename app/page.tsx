07e3ba0b3efb1beedff2745dd585f704bc4429fd
Divya Pahuja <divyapahuja31@gmail.com>
2026-07-06 11:31:18 +0530

    feat: add sidebar layout and update background styling in page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import React from 'react';
 import { ReactFlow, useNodesState, useEdgesState, Background } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 
@@ -8,16 +9,22 @@ export default function FlowPage() {
   const [edges, , onEdgesChange] = useEdgesState([]);
 
   return (
-    <main className="w-screen h-screen">
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        fitView
-      >
-        <Background />
-      </ReactFlow>
+    <main className="flex w-screen h-screen overflow-hidden bg-slate-50 dark:bg-zinc-950">
+      {/* Sidebar */}
+      <aside className="w-64 border-r border-slate-200 bg-white dark:border-zinc-800 dark:bg-zinc-900 shrink-0" />
+
+      {/* React Flow Canvas */}
+      <div className="flex-1 h-full relative">
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          fitView
+        >
+          <Background />
+        </ReactFlow>
+      </div>
     </main>
   );
 }
