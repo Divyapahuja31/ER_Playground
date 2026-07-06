f6a7d7f091512b238096c8f3b42d96efc38483d2
Kanishk Ranjan <kanishkranjan17@gmail.com>
2026-07-06 14:46:45 +0530

    feat: integrate Sidebar into Canvas component and adjust layout

--- app/components/canvas.tsx
+++ app/components/canvas.tsx
@@ -1,33 +1,38 @@
 import React from 'react';
-import { ReactFlow, useNodesState, useEdgesState, Background, Controls, SelectionMode } from '@xyflow/react';
+import { ReactFlow, useNodesState, useEdgesState, Background, Controls, SelectionMode, ReactFlowProvider } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
+import Sidebar from './sidebar';
 
 function Canvas() {
   const [nodes, , onNodesChange] = useNodesState([]);
   const [edges, , onEdgesChange] = useEdgesState([]);
 
   return (
-    <div className="flex-1 h-full relative bg-white">
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        panOnDrag={[1, 2]}
-        selectionOnDrag={true}
-        selectionMode={SelectionMode.Partial}
-        fitView
-      >
-        <Background />
+    <ReactFlowProvider>
+      <div className="flex w-full h-full relative bg-white">
+        <Sidebar />
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          panOnDrag={[1, 2]}
+          selectionOnDrag={true}
+          selectionMode={SelectionMode.Partial}
+          fitView
+        >
+          <Background />
+          <Controls
+            style={{ color: "black" }}
+            position="bottom-right"
+            showFitView={false}
+            showInteractive={false}
+          />
+        </ReactFlow>
+      </div>
 
-        <Controls
-        style={{color:"black"}}
-          position="bottom-right"
-          showFitView={false}      
-          showInteractive={false} 
-        />
-      </ReactFlow>
-    </div>
+    </ReactFlowProvider>
+
   );
 }
 
--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -3,7 +3,7 @@ import SidebarTab from './sidebarTab';
 import SidebarInnerView from './sidebarInnerView';
 const Sidebar = () => {
   return (
-    <div className="w-80 border-r border-slate-200 bg-white gray:border-zinc-800 light:bg-zinc-900 shrink-0">
+    <div className="w-80 h-full border-r border-slate-200 bg-white gray:border-zinc-800 light:bg-zinc-900 shrink-0">
       <div className='w-auto h-10 flex'>
         <SidebarTab name='Tables'/>
         <SidebarTab name='Relationships'/>
